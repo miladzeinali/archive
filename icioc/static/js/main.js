cd21b605a65b96e631e2bbdbd632a29af72a513e
milad zeinali <58866441+miladzeinali@users.noreply.github.com>
2023-08-05 11:49:57 +0330

    add new js and css

--- icioc/static/js/main.js
+++ icioc/static/js/main.js
@@ -1,11 +1,45 @@
-$(function () {
-    $('.genealogy-tree ul').hide();
-    $('.genealogy-tree>ul').show();
-    $('.genealogy-tree ul.active').show();
-    $('.genealogy-tree li').on('click', function (e) {
-        var children = $(this).find('> ul');
-        if (children.is(":visible")) children.hide('fast').removeClass('active');
-        else children.show('fast').addClass('active');
-        e.stopPropagation();
+'use strict';
+
+(function($){
+
+  $(function() {
+
+    var datascource = {
+      'name': 'Lao Lao',
+      'title': 'general manager',
+      'children': [
+        { 'name': 'Bo Miao', 'title': 'department manager', 'className': 'middle-level',
+          'children': [
+            { 'name': 'Li Jing', 'title': 'senior engineer', 'className': 'product-dept' },
+            { 'name': 'Li Xin', 'title': 'senior engineer', 'className': 'product-dept',
+              'children': [
+                { 'name': 'To To', 'title': 'engineer', 'className': 'pipeline1' },
+                { 'name': 'Fei Fei', 'title': 'engineer', 'className': 'pipeline1' },
+                { 'name': 'Xuan Xuan', 'title': 'engineer', 'className': 'pipeline1' }
+              ]
+            }
+          ]
+        },
+        { 'name': 'Su Miao', 'title': 'department manager', 'className': 'middle-level',
+          'children': [
+            { 'name': 'Pang Pang', 'title': 'senior engineer', 'className': 'rd-dept' },
+            { 'name': 'Hei Hei', 'title': 'senior engineer', 'className': 'rd-dept',
+              'children': [
+                { 'name': 'Xiang Xiang', 'title': 'UE engineer', 'className': 'frontend1' },
+                { 'name': 'Dan Dan', 'title': 'engineer', 'className': 'frontend1' },
+                { 'name': 'Zai Zai', 'title': 'engineer', 'className': 'frontend1' }
+              ]
+            }
+          ]
+        }
+      ]
+    };
+
+    var oc = $('#chart-container').orgchart({
+      'data' : datascource,
+      'nodeContent': 'title'
     });
-});
+
+  });
+
+})(jQuery);
