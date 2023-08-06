103c4ff9cff6916229c2132a6ce2b9a796a26f49
shiateam <mhmdsadqd@gmail.com>
2023-08-06 07:12:42 +0330

    add to classs in model and add view and change template

--- coding/admin.py
+++ coding/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import *
+from mptt.admin import MPTTModelAdmin
+
+admin.site.register(Category,MPTTModelAdmin)
+admin.site.register(Product,admin.ModelAdmin)
 
 class ZoneAdmin(admin.ModelAdmin):
     list_display = ('zone',)
