7107ede2d9987703afdd550eed196909b8827492
milad zeinali <58866441+miladzeinali@users.noreply.github.com>
2023-09-26 12:18:09 +0330

    before pull

--- coding/urls.py
+++ coding/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('addingcontrol/<obj>/',AddRedirect,name='addingredirect'),
     path('samelevel/<int:id>/',AddSameLevel,name='addsamelevel'),
     path('get_parent_node/',get_parent_node,name='get-parent-node'),
+    path('caption/',Caption,name='caption'),
+    path('updatecaption/',UpdateCaption,name='updatecaption'),
     path('search/',Search,name='search')
 ]
